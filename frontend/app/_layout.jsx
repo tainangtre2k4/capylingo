9feb95fa33cc650495d06c028491b49d9fa135e2
TaiNangTre2k4 <ndt280504@gmail.com>
2024-08-12 02:09:48 +0700

    Sync Clerk vs Supabase

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -9,14 +9,14 @@ const tokenCache = {
   async getToken(key) {
     try {
       const item = await SecureStore.getItemAsync(key);
-      if (item) {
-        console.log(`${key} was used 🔐 \n`);
-      } else {
-        console.log("No values stored under key: " + key);
-      }
+      // if (item) {
+      //   console.log(`${key} was used 🔐 \n`);
+      // } else {
+      //   console.log("No values stored under key: " + key);
+      // }
       return item;
     } catch (error) {
-      console.error("SecureStore get item error: ", error);
+      //console.error("SecureStore get item error: ", error);
       await SecureStore.deleteItemAsync(key);
       return null;
     }
---
+++ frontend/supabase/functions/create-user/index.ts
@@ -0,0 +1,44 @@
+import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@1.30.0';
+
+const supabaseUrl = Deno.env.get('SUPABASE_URL')!;
+const supabaseServiceKey = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!;
+
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+serve(async (req: Request) => {
+  if (req.method !== 'POST') {
+    return new Response('Method not allowed', { status: 405 });
+  }
+
+  try {
+    const { id, email_addresses, first_name, image_url,last_name } = (await req.json()).data;
+    const email = email_addresses[0].email_address;
+    const insertPayload = {
+      id,
+      email,
+      avatar_url: image_url,
+      first_name,
+      last_name,
+    };
+    console.log('Insert Payload:', insertPayload);
+    const { data, error } = await supabase
+      .from('users')
+      .insert(insertPayload);
+    if (error) {
+      return new Response(JSON.stringify(error), { status: 400 });
+    }
+
+    return new Response(JSON.stringify(data), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 201,
+    });
+  } catch (err) {
+    console.log(err);
+
+    return new Response(JSON.stringify({ error: err.message }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    });
+  }
+});
