c0b22efac4e32e3711d11525c330fdc70ba785fa
TaiNangTre2k4 <ndt280504@gmail.com>
2024-08-30 06:48:02 +0700

    comment things not use

--- frontend/app/(auth)/home.tsx
+++ frontend/app/(auth)/home.tsx
@@ -4,20 +4,20 @@ import { useUser } from '@clerk/clerk-expo';
 import useTopicList from '../../src/api/level/index'
 const Home = () => {
   const { user } = useUser();
-  const { data, isLoading, error } = useTopicList();
+  // const { data, isLoading, error } = useTopicList();
 
-  if (isLoading) {
-    return <ActivityIndicator />;
-  }
-  if (error) {
-    return <Text>Failed to fetch topics</Text>;
-  }
-  console.log(data)
+  // if (isLoading) {
+  //   return <ActivityIndicator />;
+  // }
+  // if (error) {
+  //   return <Text>Failed to fetch topics</Text>;
+  // }
+  // console.log(data)
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome, {user?.emailAddresses[0].emailAddress} 🎉</Text>
-      <Text>{data[0].title}</Text>
+      {/* <Text>{data[0].title}</Text> */}
     </View>
   );
 };
