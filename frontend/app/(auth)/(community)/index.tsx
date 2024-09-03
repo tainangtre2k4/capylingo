ef514e4fa4b3525af7cd39c52c93a00bd319203c
TaiNangTre2k4 <ndt280504@gmail.com>
2024-09-03 16:03:48 +0700

    Revert "comment feature"
    This reverts commit 46a1d49697546437eea80afd95df4591215afe63.

--- frontend/app/(auth)/(community)/index.tsx
+++ frontend/app/(auth)/(community)/index.tsx
@@ -1,6 +1,6 @@
-import { View, FlatList, Alert } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import posts from '../../../assets/data/posts.json';
+import { View,FlatList, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import posts from '../../../assets/data/posts.json'
 import Header from '@/components/news/Header';
 import PostListItem from '@/components/community/PostListItem';
 import { useRouter } from 'expo-router';
@@ -10,58 +10,55 @@ import { useUser } from '@clerk/clerk-expo'; // Clerk for authentication
 const FeedScreen = () => {
   const { user } = useUser(); // Get authenticated user from Clerk
 
-  const [posts, setPosts] = useState([]);
+  const [posts,setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  useEffect(()=>{
     fetchPosts();
-  }, []);
+  },[])
 
-  const fetchPosts = async () => {
+  const fetchPosts = async () =>{
     setLoading(true);
     let { data, error } = await supabase
-      .from('posts')
-      .select('*,user:users(*),my_likes:likesPost(*),likesPost(count)')
-      .eq('my_likes.user_id', user?.id);
+    .from('posts')
+    .select('*,user:users(*),my_likes:likesPost(*),likesPost(count)') 
+    .eq('my_likes.user_id', user?.id) 
     if (error) {
-      Alert.alert("Something went Wrong !");
+      Alert.alert("Something went Wrong !")
     }
+    console.log(data)
     const sortedPosts = data.sort((a, b) => b.id - a.id);
     setPosts(sortedPosts);
     setLoading(false);
-  };
 
-  const router = useRouter();
+  }
 
-  const commentHandler = (post) => {
-    router.push(`/comment?postId=${post.id}`);
-  };
-
-  return (
+  const router =useRouter();
+  
+  return(
     <View>
-      <Header
-        title='Community'
-        backHandler={() => {}}
-        navigateHandler={() => { router.push('/create') }}
-        search={false}
-        create={true}
+      <Header 
+      title='Comunity' backHandler={() => {}} navigateHandler={()=>{router.push('/create')}}
+      search={false} 
+      create={true}
       />
-
+      
       <FlatList
-        data={posts}
-        renderItem={({ item }) => <PostListItem post={item} commentHandler={commentHandler} />}
-        contentContainerStyle={{
-          gap: 10,
-          maxWidth: 512,
-          alignSelf: 'center',
-          width: '100%',
-        }}
-        showsVerticalScrollIndicator={false}
-        onRefresh={fetchPosts}
-        refreshing={loading}
-      />
+      data={posts}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      contentContainerStyle={{
+        gap: 10,
+        maxWidth: 512,
+        alignSelf: 'center',
+        width: '100%',
+      }}
+      showsVerticalScrollIndicator={false}
+      onRefresh={fetchPosts}
+      refreshing={loading}
+    />
+      
     </View>
-  );
-};
+  )
+}
 
-export default FeedScreen;
+export default FeedScreen
--- frontend/components/community/PostListItem.tsx
+++ frontend/components/community/PostListItem.tsx
@@ -12,88 +12,100 @@ import { useUser } from '@clerk/clerk-expo'; // Clerk for authentication
 import { supabase } from '@/lib/supabase';
 import { sendLikeNotification } from '@/utils/Notification';
 
-export default function PostListItem({ post, commentHandler,show=true }) {
-  const { user } = useUser(); // Get authenticated user from Clerk
-  const [isLiked, setIsLiked] = useState(false);
-  const [likeRecord, setLikeRecord] = useState(null);
+export default function PostListItem({post}){
+    const { user } = useUser(); // Get authenticated user from Clerk
+    const [isLiked,setIsLiked]= useState(false);
+    const [likeRecord,setLikeRecord] = useState(null)
 
-  useEffect(() => {
-    if (post.my_likes.length > 0) {
-      setLikeRecord(post.my_likes[0]);
-      setIsLiked(true);
-    }
-  }, [post.my_likes]);
+    useEffect(()=>{
+      if (post.my_likes.length > 0) {
+        setLikeRecord(post.my_likes[0]);
+        setIsLiked(true);
+      }
+    },[post.my_likes])
 
-  useEffect(() => {
-    if (isLiked) {
-      saveLike();
-    } else deleteLike();
-  }, [isLiked]);
+    useEffect(()=>{
+      if (isLiked) {
+        saveLike();
+      }
+      else deleteLike();
+    },[isLiked])
 
-  const saveLike = async () => {
-    if (likeRecord) return;
-    const { data, error } = await supabase.from('likesPost').insert([
-      {
+    // const fetchLike = async () => {
+    //   const {data} = await supabase.from('likesPost').select('*')
+    //   .eq('user_id',user?.id)
+    //   .eq('post_id',post.id)
+    //   .select()
+    //   if (data && data?.length>0)
+    //   {
+    //     setLikeRecord(data[0])
+    //     setIsLiked(true)
+    //   }
+    // }
+
+    const saveLike = async ()  => {
+      if (likeRecord) return;
+      const {data,error} = await supabase.from('likesPost').insert([{
         user_id: user?.id,
         post_id: post.id,
-      },
-    ]).select('*');
-    if (error) console.log(error);
-    sendLikeNotification(data[0]);
-    if (data) {
-      console.log(data[0]);
-      setLikeRecord(data[0]);
+      }]).select('*')
+      if (error) console.log(error)
+      sendLikeNotification(data[0])
+      if (data)  {console.log(data[0]); setLikeRecord(data[0]);}
     }
-  };
 
-  const deleteLike = async () => {
-    if (likeRecord) {
-      const { error } = await supabase.from('likesPost').delete().eq('id', likeRecord.id);
-      if (!error) setLikeRecord(null);
+    const deleteLike = async ()  => {
+      if (likeRecord){
+        const {error} = await supabase.from('likesPost').delete().eq('id',likeRecord.id)
+        if (!error) setLikeRecord(null);
+      }
+     
     }
-  };
 
-  const avatarUrl = post.user.avatar_url;
-  const avatar = cld.image(
-    avatarUrl && avatarUrl.startsWith('https://img.clerk.com/')
-      ? 'user_ynfjc7'
-      : avatarUrl || 'user_ynfjc7'
-  );
-  avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())));
+    const avatarUrl = post.user.avatar_url;
+    const avatar = cld.image(
+      avatarUrl && avatarUrl.startsWith('https://img.clerk.com/')
+        ? 'user_ynfjc7'
+        : avatarUrl || 'user_ynfjc7'
+    );
+    avatar.resize(thumbnail().width(48).height(48).gravity(focusOn(FocusOn.face())));
+    
 
-  return (
-    <View className='bg-white'>
-      <View className='p-3 flex-row items-center gap-2'>
-        <AdvancedImage
-          cldImg={avatar}
-          className='w-12 aspect-square rounded-full'
-        />
-        <Text className='font-semibold '>{post.user.username || 'New User'}</Text>
-      </View>
+    return (
+        <View className='bg-white'>
+          
+    
+    
+          <View className='p-3 flex-row items-center gap-2'>
+              <AdvancedImage
+                cldImg={avatar}
+                className='w-12 aspect-square rounded-full'
+              />
+              <Text className='font-semibold '>{post.user.username || 'New User'}</Text>
+          </View>
 
-      <PostContent post={post} />
+          <PostContent post={post}/>
+          
 
-      {/* Icons */}
-      {show && <View className="flex-row gap-3 p-3">
-        <AntDesign
-          onPress={() => setIsLiked(!isLiked)}
-          name={isLiked ? "heart" : "hearto"}
-          size={20}
-          color={isLiked ? 'crimson' : 'black'}
-        />
-        <Ionicons onPress={() => commentHandler(post)} name="chatbubble-outline" size={20} />
-        <Feather name="send" size={20} />
+          {/* Icons */}
+          <View className="flex-row gap-3 p-3">
+            <AntDesign onPress={()=>setIsLiked(!isLiked)}
+            name={isLiked? "heart" :"hearto" } size={20} 
+            color={isLiked? 'crimson': 'black' }/>
+            <Ionicons name="chatbubble-outline" size={20} />
+            <Feather name="send" size={20} />
+    
+            <Feather name="bookmark" size={20} className="ml-auto" />
+          </View>
 
-        <Feather name="bookmark" size={20} className="ml-auto" />
-      </View>}
-
-      <View className='px-3 gap-1'>
-        {show && <Text className='font-semibold'>{post.likesPost?.[0]?.count || 0} likes</Text>}
-        <Text>
-          <Text className='font-semibold'>{post.user.username} </Text>
-          {post.caption}
-        </Text>
-      </View>
-    </View>
-  );
+          <View className='px-3 gap-1'>
+            <Text className='font-semibold'>{post.likesPost?.[0]?.count || 0} likes</Text>
+            <Text>
+              <Text className='font-semibold'>{post.user.username} </Text>
+              {post.caption}
+            </Text>
+          </View>
+    
+        </View>
+    )
 }
